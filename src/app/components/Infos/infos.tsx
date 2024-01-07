f7adf8d5b0adf66434ccc03c30274900da7eac3f
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-07 10:44:46 -0300

    feat: change the position of education

--- src/app/components/Infos/infos.tsx
+++ src/app/components/Infos/infos.tsx
@@ -4,16 +4,16 @@ import './infos.scss'
 export function Infos() {
 return (
     <div className="informations">
-        <SectionTitle text="Idiomas"/>
-        <div className="languages-info">
-        <span>🇺🇸 EN - Intermediário</span>
-        <span>🇧🇷 PT-BR - Nativo</span>
-        </div>
         <SectionTitle text="Educação" />
         <div className="educational-info">
         <span>🎓 Ciência da Computação - Estácio</span>
         <span>📚 Desenvolvedor Full Stack Python - EBAC</span>
         <span>📚 Bootcamp Desenvolvimento Frontend com Angular - DIO</span>
+        </div>
+        <SectionTitle text="Idiomas"/>
+        <div className="languages-info">
+        <span>🇺🇸 EN - Intermediário</span>
+        <span>🇧🇷 PT-BR - Nativo</span>
         </div>
     </div>
 )
