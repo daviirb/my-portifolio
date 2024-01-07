afa0314afb7497eed51f2505520424a48a999215
Davi Ribeiro <davi.rb@hotmail.com>
2024-01-07 10:44:02 -0300

    feat: Add about text

--- src/app/components/About/about.tsx
+++ src/app/components/About/about.tsx
@@ -5,7 +5,9 @@ export function About(){
     return(
         <div className="about">
         <SectionTitle text="Sobre mim" />
-        <p>Sou Desenvolvedor Front-end e estou me graduando em Ciência da Computação.</p>
+        <p>Como desenvolvedor front-end, transformo linhas de código em experiências visuais, buscando constantemente a inovação e aprimoramento.</p>
+        <p>Minha jornada acadêmica não é apenas um percurso, mas sim o alicerce sólido que sustenta minha paixão pela tecnologia, proporcionando um conhecimento profundo e uma base robusta.</p>
+        <p><q>A tecnologia move o mundo e a criatividade o faz girar.</q> - Neil Armstrong</p>
         </div>  
     )
 }
